e404b6efd0e52b0f36a4808b1e9f0105d50c2ff0
carlossdf1 <carlos.sdf1@gmail.com>
2021-04-23 01:45:27 -0400

    funcion imprimir, boton flotante

--- js/helpers/function.js
+++ js/helpers/function.js
@@ -51,10 +51,17 @@ function imprimirLista(datos) { //imprime los datos entregados en lista html
             data.quantity + td +
             data.category + td +
             data.ubication + td +
-            data.observations +
+            elementoVacio(data.observations) +
             '</td></tr>';
     }
 
+}
+
+function elementoVacio(dato) {
+    if (dato === undefined) {
+        dato = "";
+    }
+    return dato;
 }
 
 function buscar() { //busca las concidencias de la busqueda y una lista de resultados
@@ -92,4 +99,18 @@ function buscar() { //busca las concidencias de la busqueda y una lista de resul
 
     return resultadoBusqueda
 
+}
+
+function imprimirElemento() {
+    var elemento = document.getElementById('tabla');
+    var ventana = window.open('', 'PRINT', 'height=400,width=600');
+    ventana.document.write('<html><head><title>' + document.title + '</title>');
+    ventana.document.write('</head><body>');
+    ventana.document.write(elemento.innerHTML);
+    ventana.document.write('</body></html>');
+    ventana.document.close();
+    ventana.focus();
+    //ventana.print();
+    //ventana.close();
+    return true;
 }
