f58ce6334774420f9bf0d7bc3bbdc66783200683
carlossdf1 <carlos.sdf1@gmail.com>
2021-06-20 19:39:13 -0400

    Reduccion de codigo y lista de usuarios

--- js/users/users.js
+++ js/users/users.js
@@ -9,3 +9,32 @@ async function consultaUsuarios() {
     let listaUsuarios   = respuesta.data;
     return  listaUsuarios;
 };
+
+function listaUsuarios( datos ) { //imprime los datos entregados en lista html
+    console.log("DATOS RECIBIDOS");
+    /* console.log(datos); */
+ 
+    for (let i in datos) {
+        const data = datos[i];
+        console.log(data);
+        document.getElementById("userNombre").innerHTML=data.name;
+        document.getElementById("userDatos").innerHTML=data.email + "<br>" + data.role;
+        document.getElementById("userEstado").innerHTML=data.online;
+
+        let card = document.getElementById("CardUser");
+        console.log(i);
+        console.log(datos.length-1);
+        if (i==datos.length-1) {
+            document.getElementById("listaUsuarios").innerHTML += card.outerHTML;
+            document.getElementById("CardUser").className += " d-none";
+        } else {
+            document.getElementById("listaUsuarios").innerHTML += card.outerHTML;
+
+        }
+    }
+
+  }
+
+  async function imprimir() { 
+    listaUsuarios( await consultaUsuarios( ) );  
+  }
