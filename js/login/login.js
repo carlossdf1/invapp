f98c678bd650e99e6063cba38ef0f6b35c2571f2
Emmanuel Correa <ebcorreac@gmail.com>
2021-07-18 01:49:45 -0400

    cambio en login con localStorage

--- js/login/login.js
+++ js/login/login.js
@@ -49,11 +49,14 @@ async function REST(data, route, method) {
                 // if (isChecked) {
 
                 // }
-
-                localStorage.setItem("login", JSON.stringify(resp.data));
+                console.log(resp.data);
+                // localStorage.setItem("login", JSON.stringify(resp.data));
                 localStorage.setItem("token", resp.token);
                 localStorage.setItem("email", resp.data.email);
                 localStorage.setItem("username", resp.data.name);
+                localStorage.setItem("role", resp.data.role._id);
+                localStorage.setItem("menu", JSON.stringify(resp.data.role.menuOptions));
+                localStorage.setItem("group", JSON.stringify(resp.data.role.groupOptions));
 
                 showAlert(resp.msg);
 
