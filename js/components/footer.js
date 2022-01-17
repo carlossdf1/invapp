a7d9652b0bcf190213308769c84a680057cb0333
carlossdf1 <carlos.sdf1@gmail.com>
2022-01-16 23:44:02 -0300

    actulizacion año

--- js/components/footer.js
+++ js/components/footer.js
@@ -53,7 +53,7 @@ class Footer extends HTMLElement {
             <a href="/WebStore/view/nosotros/nosotros.html">Nosotros</a>
           </section>
           <div class="copyright">
-            © Derechos Reservados 2021 | <a href="https://impulsandonegocios.cl">ImpulsandoNegocios</a>
+            © Derechos Reservados 2022 | <a href="https://impulsandonegocios.cl">ImpulsandoNegocios</a>
           </div>
         </footer>
 
