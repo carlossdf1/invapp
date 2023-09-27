3adf4f9c79823b38f16f3fc8f89e7f6aa96de0b3
Emmanuel Correa <ebcorreac@gmail.com>
2023-09-27 00:44:39 -0300

    [EDIT] Changes in index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
-/**
- * Función que muestra cada linea de informacion
- *
- * @author Emmanuel Correa <ebcorrea[at]gmail.com>
- * @version 2021-05-06
- */
-
 const username = localStorage.getItem("username");
 
-async function consultaData(){
-    
+async function consultaData() {
     const urlProds = ( roleName == 'admin' || group.length > 1 ) ? api +  "product/products" : api + `product/products/?group=${ group[0].name }`;
-    const respuesta = await consulta(urlProds);
+    const respuesta = await consulta( urlProds );
     const filtro = respuesta.data.filter( ( item ) =>  item.group !== "Eliminados" && item.group !== "Prestamos" && item.active === true );
     localStorage.setItem("productosTotal", JSON.stringify( respuesta.data ));
     localStorage.setItem("productos", JSON.stringify( filtro ));
@@ -19,7 +11,6 @@ async function consultaData(){
     const productosPrestados = dataTotalProd.filter( ( item ) => item.group === "Prestamos" );
     const productosEliminados = dataTotalProd.filter( ( item ) => item.group === "Eliminados" );
 
-
     if ( username ) {
         document.querySelector('#title-index').innerHTML = `${ username } : Aqui podras ver los datos en tiempo real`;
         ( roleName == 'admin' ) ? console.log('Feature Flags de botones en INDEX') : hiddenElements();
@@ -27,23 +18,21 @@ async function consultaData(){
         document.getElementById("numeroProductos").innerHTML = dataProdFiltered.length;
         
         if( roleName == 'admin' ) {
-            document.getElementById("numeroPrestados").innerHTML  =  productosPrestados.length;
-            document.getElementById("numeroEliminados").innerHTML =  productosEliminados.length; 
-        
+            document.getElementById("numeroPrestados").innerHTML = productosPrestados.length;
+            document.getElementById("numeroEliminados").innerHTML = productosEliminados.length; 
         } else {
-            
             hiddenElements();
-            document.getElementById("numeroProductos").innerHTML  =  dataProdFiltered.length;
-            document.getElementById("numeroPrestados").innerHTML  =  productosPrestados.length;
-            document.getElementById("numeroEliminados").innerHTML =  productosEliminados.length; 
-            
+            document.getElementById("numeroProductos").innerHTML = dataProdFiltered.length;
+            document.getElementById("numeroPrestados").innerHTML = productosPrestados.length;
+            document.getElementById("numeroEliminados").innerHTML = productosEliminados.length;    
         }
     }
 }
-
-consultaData();
 
 function hiddenElements() {
     dNone('btn-prestado',  false );
     dNone('btn-eliminado', false );
 }
+
+darkModeChange();
+consultaData();
